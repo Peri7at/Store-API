c38b4d55d30bd4fe3ddeebe2bf912b6d914286f4
Peri7at <91065358+Peri7at@users.noreply.github.com>
2022-04-14 23:49:49 +0300

    add db query

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,40 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
-  res.status(200).json({products})
+  const products = await Product.find({
+    featured: true
+  });
+  res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({msg: 'Products route'})
+  const {featured, company, name, sort} = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  };
+
+  if (company) {
+    queryObject.company = company;
+  };
+
+  if (name) {
+    queryObject.name = {$regex: name, $options: 'i'};
+  };
+
+  let result = Product.find(queryObject);
+
+  if(sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt'); 
+  }
+ 
+  const products = await result;
+
+  res.status(200).json({products, nbHits: products.length});
 };
 
 module.exports = { getAllProductsStatic, getAllProducts}
